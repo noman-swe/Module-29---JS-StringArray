ee72f805831e7b8d1a05e0821e4588ee9735659d
noman-swe <noman.swe130@gmail.com>
2022-08-14 00:27:41 +0600

    29-2: search with indexOf()

---
+++ JS-StringArray/string-search.js
@@ -0,0 +1,21 @@
+const products = [
+    'Dell hardcore i29 200GB laptop',
+    'iphone 1TB camera flashlight phone',
+    'yellow laptop with black camera',
+    '1X59 Lenovo commercial Laptop',
+    'LG supernova laptop',
+    'HTC low price Phone',
+    'Purple color phone with laptop'
+];
+
+const searching = 'laptop';
+
+// indexOf
+const output = [];
+for (const product of products) {
+    if (product.toLowerCase().indexOf(searching) != -1) {
+        output.push(product);
+    }
+}
+
+console.log(output);
