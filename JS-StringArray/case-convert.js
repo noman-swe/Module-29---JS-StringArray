3ac170f7b991a8f89a968fcc2da735b803c41beb
noman-swe <noman.swe130@gmail.com>
2022-08-13 23:44:37 +0600

    first commit

---
+++ JS-StringArray/case-convert.js
@@ -0,0 +1,10 @@
+const anthem = 'amr shonar bangla ami Tomay valobashi';
+const searching = 'valo';
+
+const userInput = 'blackPinK';
+const savedUserName = 'blackPink'; 
+if(userInput == savedUserName){
+    console.log('matched');
+}else{
+ console.log('not matched');
+}
