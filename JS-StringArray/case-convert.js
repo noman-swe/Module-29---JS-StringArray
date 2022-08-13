62bcf7519737469fcaade1024204498c0b8a5b68
noman-swe <noman.swe130@gmail.com>
2022-08-13 23:51:32 +0600

    29-1 String comparison using toLowerCase toUpperCase

--- JS-StringArray/case-convert.js
+++ JS-StringArray/case-convert.js
@@ -1,9 +1,9 @@
-const anthem = 'amr shonar bangla ami Tomay valobashi';
+const anthem = 'Amr Shonar Bangla Ami Tomay Valobashi';
 const searching = 'valo';
 
 const userInput = 'blackPinK';
 const savedUserName = 'blackPink'; 
-if(userInput == savedUserName){
+if(userInput.toLowerCase() == savedUserName.toLowerCase()){
     console.log('matched');
 }else{
  console.log('not matched');
