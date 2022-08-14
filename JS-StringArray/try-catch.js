625890b691dadab5efca760cb06d963b2f8916c5
noman-swe <noman.swe130@gmail.com>
2022-08-15 00:36:53 +0600

    29-8 Different types of Error object and explore Try-catch

---
+++ JS-StringArray/try-catch.js
@@ -0,0 +1,28 @@
+//typeError
+const name = 'ami';
+// name = 'Shibly';
+// console.log(name);
+
+
+//syntaxError
+/* for(let i = 0; i < 10 i++){
+
+}
+ */
+
+const student = undefined;
+// console.log(student.name);
+
+
+// 
+// 
+// 
+const friendsName = 'Noor';
+
+try{
+    friendsName = 'Noman';
+}catch(error){
+    // console.log(error);
+}
+
+console.log(friendsName);
