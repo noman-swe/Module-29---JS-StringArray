aa2b7db52c296dfc657c8393643a866c4980fc48
noman-swe <noman.swe130@gmail.com>
2022-08-14 10:48:40 +0600

    29-3:- slice()

--- JS-StringArray/string-slice.js
+++ JS-StringArray/string-slice.js
@@ -1,4 +1,11 @@
 const anthem = 'Amar Shonar Bangla Ami Tomay Valobashi';
+// split()
 const words = anthem.split(' ');
 const withoutA = anthem.toLowerCase().split('a');
-console.log(withoutA);
+// console.log(withoutA);
+
+// slice()
+const starsAfterFive = anthem.slice(5);
+const starsAfter18 = anthem.slice(18);
+console.log(starsAfterFive);
+console.log(starsAfter18);
